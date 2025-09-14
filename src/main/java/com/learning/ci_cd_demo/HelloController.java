67c5286c83022c23d7b5ed6aa3392fc99100ec7e
Mohan Lal <mhnuk2007@gmail.com>
2025-09-14 23:43:44 +0500

    add code after rectifying error

--- src/main/java/com/learning/ci_cd_demo/HelloController.java
+++ src/main/java/com/learning/ci_cd_demo/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String greet(){
+    public String greet() {
         return "Hello World!";
     }
+}
